0cd4b7af2b0bc7dd725931d2bde9f87d5dc40989
Daniel Diaz <danie.infante@itexico.com>
2019-07-25 12:52:57 -0500

    room interaction reworked

--- index.js
+++ index.js
@@ -7,26 +7,24 @@ const io = require('socket.io')(http)
 const bodyParser = require('body-parser')
 const uuid = require('uuid/v4')
 
-let rooms = []
-let messages = []
-let userCount = 0
-
 // middleware
 app.use(cors())
 app.use(bodyParser.json())
 
+let rooms = []
+let messages = []
+let userCount = 0
+
 app.get('/room/:roomId', (req, res) => {
   const room = rooms.find(room => {
-    return room.roomId = req.params.roomId
+    return room.roomId === req.params.roomId
   })
-  res.status(200).json(room)
-})
 
-app.get('/chatinfo', (req, res) => {
-  res.status(200).json({
-    onlineUsers: userCount,
-    totalMsgs: messages.length
-  })
+  if (room) {
+    res.status(200).json(room)
+  } else {
+    res.status(404).json({ msg: 'room not found' })
+  }
 })
 
 app.get('/rooms', (req, res) => {
@@ -36,19 +34,19 @@ app.get('/rooms', (req, res) => {
 app.post('/room', (req, res) => {
   const { roomId, roomName, roomOwner } = req.body
 
-  if (!roomId || !roomName || !roomOwner) res.status(400).json({ msg: 'fields missing' })
+  if (!roomId || !roomName || !roomOwner) {
+    res.status(400).json({ msg: 'fields missing' })
+  } else {
+    const uid = uuid()
+    const newRoom = { roomId: uid, roomName, roomOwner, messages: [], onlineUsers: [] }
+    rooms = [...rooms, newRoom]
 
-  const uid = uuid()
-  const newRoom = { roomId: uid, roomName, roomOwner, messages: [], onlineUsers: [] }
-  rooms = [...rooms, newRoom]
-  console.log(newRoom)
-  res.status(200).json({ msg: 'room created', room: newRoom })
+    res.status(200).json({ msg: 'room created', room: newRoom })
+  }
 })
 
 //chat socket
 io.on('connection', socket => {
-  console.log('user connected')
-
   // room creation
   socket.on('room-created', () => {
     // emit room list
@@ -61,38 +59,44 @@ io.on('connection', socket => {
     io.emit('chat-list', rooms)
   })
 
+  // room joined
+  socket.on('join-room', ({ roomId, user }) => {
+    if (!roomId || !user) return
+
+    const roomIndex = rooms.findIndex(room => {
+      return room.roomId === roomId
+    })
+
+    if (roomIndex >= 0) {
+      rooms[roomIndex].onlineUsers = [...rooms[roomIndex].onlineUsers, user]
+      io.emit('roomInfo-' + roomId, rooms[roomIndex])
+      io.emit('chat-list', rooms)
+    }
+  })
+
+  // room left
+  socket.on('leave-room', ({ roomId, user }) => {
+    if (!roomId || !user) return
+
+    const roomIndex = rooms.findIndex(room => {
+      return room.roomId === roomId
+    })
+
+    if (roomIndex >= 0) {
+      rooms[roomIndex].onlineUsers = rooms[roomIndex].onlineUsers.filter(u => u.uid !== user.uid)
+      io.emit('roomInfo-' + roomId, rooms[roomIndex])
+      io.emit('chat-list', rooms)
+    }
+  })
+
   // message
   socket.on('msg', msg => {
     // add message to room obj
     const roomIndex = rooms.findIndex(room => room.roomId === msg.roomId)
     rooms[roomIndex].messages = [...rooms[roomIndex].messages, msg]
-
     // emmit message
     io.emit('msg-' + msg.roomId, msg)
   })
-
-  // userCount++
-  // io.emit('chatInfo', {
-  //   onlineUsers: userCount,
-  //   totalMsgs: messages.length
-  // })
-
-  // socket.on('msg', msg => {
-  //   messages.push(msg)
-  //   io.emit('newMsg', msg)
-  //   io.emit('chatInfo', {
-  //     onlineUsers: userCount,
-  //     totalMsgs: messages.length
-  //   })
-  // })
-
-  // socket.on('disconnect', (a) => {
-  //   userCount--
-  //   io.emit('chatInfo', {
-  //     onlineUsers: userCount,
-  //     totalMsgs: messages.length
-  //   })
-  // })
 })
 
 // server running
