ba2dcd6cd8f90e611d35122b0c711a689533cb21
Daniel Diaz <danie.infante@itexico.com>
2019-08-01 15:00:56 -0500

    direct message on progress

--- index.js
+++ index.js
@@ -12,8 +12,44 @@ app.use(cors())
 app.use(bodyParser.json())
 
 let rooms = []
-let messages = []
 let repeatedUsers = []
+
+let directRooms = []
+let onlineUsers = []
+
+app.get('/dmroom/:roomId', (req, res) => {
+  const { roomId } = req.params
+
+  const room = directRooms.find(room => room.roomId === roomId)
+
+  if (room) {
+    res.status(200).json(room)
+  } else {
+    res.status(204).json({ msg: 'no content' })
+  }
+})
+
+app.post('/dmroom', (req, res) => {
+  const newRoom = req.body
+
+  const roomExists = directRooms.find(room =>
+    (room.user1.uid === newRoom.user1.uid && room.user2.uid === newRoom.user2.uid)
+    ||
+    (room.user1.uid === newRoom.user2.uid && room.user2.uid === newRoom.user1.uid)
+  )
+
+  if (roomExists) {
+    res.status(200).json(roomExists)
+  } else {
+    newRoom.roomId = uuid()
+    directRooms = [...directRooms, newRoom]
+    res.status(200).json(newRoom)
+  }
+})
+
+app.get('/who', (req, res) => {
+  res.status(200).json(onlineUsers)
+})
 
 app.get('/room/:roomId', (req, res) => {
   const room = rooms.find(room => {
@@ -82,6 +118,21 @@ app.delete('/room/:roomId', (req, res) => {
 
 //chat socket
 io.on('connection', socket => {
+  // loged in
+  socket.on('loggedIn', who => {
+    onlineUsers = [who, ...onlineUsers]
+    io.emit('who', onlineUsers)
+  })
+
+  // loged out
+  socket.on('loggedOut', who => {
+    console.log('user logged out', who)
+    onlineUsers = onlineUsers.filter(user => user.uid !== who.uid)
+
+    console.log('onlineUsers', onlineUsers)
+    io.emit('who', onlineUsers)
+  })
+
   // room creation
   socket.on('room-created', () => {
     // emit room list
